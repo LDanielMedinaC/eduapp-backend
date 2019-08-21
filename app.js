ccc552a61a2f387f24d359d2f9296b56ef0de60e
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2019-08-21 11:18:46 -0500

    Init express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// Set up the express app
+const app = express();
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+module.exports = app;
