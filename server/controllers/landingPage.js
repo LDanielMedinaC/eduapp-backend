68cfc0f564dbad8ea9b4f92f933ed807081c8ade
Rafael Comonfort <a.comonfort@outlook.com>
2019-08-28 13:00:30 -0500

    blab bla

--- server/controllers/landingPage.js
+++ server/controllers/landingPage.js
@@ -1,4 +1,4 @@
-const User = require('../models').LandingPage;
+const LandingPage = require('../models').LandingPage;
 
 function isValid(lp){
 
@@ -8,16 +8,29 @@ module.exports = {
 
     //Method to show landing page
     show(req, res){
-        let query = LandingPage.findOne({}, {}, {sort: {'created_at' : -1}});
+
+        var query;
+        
+        //If requesting for a particular ID
+        if (req.body._id)
+            query = LandingPage.findById(req.body._id);
+        else //if no id specified, return latest
+            query = LandingPage.findOne({}, {}, {sort: {'created_at' : -1}});
 
         query.exec((err, lp) => {
-            err.status = 404;
-            err.description = "There are no landing pages";
-             
-            if (err) 
-                res.send();
             
-            res.json(lp);
+            if (err)
+            { 
+                err.status = 404;
+                err.description = "No landing page was found";
+                err.code = 1;
+                res.send(err);
+            }
+            else
+            {
+                res.status = 200;
+                res.json(lp);
+            }
         });
     },
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const userController = require("../controllers").user
-const landing_pageController = require("../controllers").landing_page
+const landing_pageController = require("../controllers").landingPage
 
 module.exports = (app) => {
+
+    //Landing Page routes
+    app.route('/landingpages')
+    .get(landing_pageController.show);
 
     //Catch all the routes. This one must always be at the end.
     app.get('*', (req, res) => res.status(200).send({
