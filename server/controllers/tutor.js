fde20c5df388c856a2f7445da46b26eb59abac6d
Syul968 <syul.pgn@gmail.com>
2019-09-04 20:21:49 -0500

    Topics seeder, tutors seeder and more scaffolding

--- server/controllers/tutor.js
+++ server/controllers/tutor.js
@@ -2,8 +2,15 @@ const User = require('../models').User;
 
 module.exports = {
     get(req, res) {
-        res.status(203).send({
-            msg: 'Should get tutors'
+        if(req.query.topic) {
+            // Look for topic id
+            // return tutors
+        }
+
+        // Return all tutors
+        User.where('tutorDetails').ne(null).exec()
+        .then(tutors => {
+            res.status(200).send(tutors);
         });
     }
 };
