a5c545a367ebe97cf1b27ce8650bb15af46f0fbb
Syul968 <syul.pgn@gmail.com>
2019-09-24 17:19:30 -0500

    Start server within test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,10 +12,26 @@ DB_URI=mongodb+srv://$DB_USER:$DB_PASS@$DB_HOST
 fi
 
 # Drop collections in DB
-mongo $DB_URI --eval "db.topics.drop();db.users.drop();db.landingpages.drop();"
+mongo $DB_URI --eval 'db.topics.drop();db.users.drop();db.landingpages.drop();'
 
 # Seed mock data
 npm run seed
 
+# Start server
+echo ''
+echo '-------STARTING SERVER'
+echo $(date)
+[[ $(lsof -t -i:8000) ]] && kill -9 $(lsof -t -i:8000)
+[[ -f nohup.out ]] && rm nohup.out
+nohup npm run start:dev &
+sleep 5
+
 # Run tests
 mocha
+
+# Log server output
+echo ''
+echo '-------SERVER LOG'
+cat nohup.out
+echo ''
+echo 'SERVER LOG-------'
