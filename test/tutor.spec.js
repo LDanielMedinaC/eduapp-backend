9f1432a6540e3f1e137527309182a6fa9517ff20
Rafael Comonfort <a.comonfort@outlook.com>
2019-09-25 13:23:44 -0500

    Added testing for getting a tutor. Not sure if they pass as nodemon keeps crashing

--- test/tutor.spec.js
+++ test/tutor.spec.js
@@ -32,3 +32,44 @@ describe('GET /tutors', () => {
         });
     });
 });
+
+describe('GET /tutors/:id', () => {
+
+    it('Not existing user', (done) => {
+       
+       let id = '000';
+
+        chai.request(server)
+        .get('/tutors/' + id)
+        .end((err, res) => {
+            res.should.have.status(400);
+            res.body.error.code.should.be.eql(2);
+            done();
+        });
+    });
+
+    it('User is not a tutor', (done) => {
+       
+        let id = '5d8b8c17dcd3c21d35198a31';
+ 
+         chai.request(server)
+         .get('/tutors/' + id)
+         .end((err, res) => {
+             res.should.have.status(400);
+             res.body.error.code.should.be.eql(3);
+             done();
+         });
+     });
+
+    it('Succesful get of a tutor', (done) => {
+    
+        let id = '5d8b8c17dcd3c21d35198a37';
+ 
+         chai.request(server)
+         .get('/tutors/' + id)
+         .end((err, res) => {
+                res.should.have.status(200);
+                done();
+         });
+     });
+});
