50bae7f17da19bf55d8763973bd2c68652adcb9e
Syul968 <syul.pgn@gmail.com>
2019-09-29 18:01:49 -0500

    Validate study institution

--- test/studies.spec.js
+++ test/studies.spec.js
@@ -148,6 +148,60 @@ describe('STUDIES', () => {
             });
             
         });
+
+        it('No study', (done) => {
+            let tutorId = noStudiesId;
+            let study = {
+                // No institution
+                degree: 'Master',
+                field: 'Aerospace',
+                grade: 90,
+                startDate: new Date('2011-07-14').toISOString(),
+                endDate: new Date('2013-12-06').toISOString(),
+                proofDocURL: 'https://storage.provider.com/items/asd78we231',
+                validationDate: new Date('2019-06-12').toISOString()
+            };
+
+            chai.request(server)
+            .post(`/tutors/${tutorId}/studies`)
+            .send(study)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.an('object');
+                res.body.should.have.property('error');
+                res.body.error.should.have.property('code');
+                res.body.error.code.should.be.eql(1);
+
+                done();
+            });
+        });
+
+        it('Study too short', (done) => {
+            let tutorId = id;
+            let study = {
+                institution: 'A',
+                degree: 'Master',
+                field: 'Aerospace',
+                grade: 90,
+                startDate: new Date('2011-07-14').toISOString(),
+                endDate: new Date('2013-12-06').toISOString(),
+                proofDocURL: 'https://storage.provider.com/items/asd78we231',
+                validationDate: new Date('2019-06-12').toISOString()
+            };
+
+            chai.request(server)
+            .post(`/tutors/${tutorId}/studies`)
+            .send(study)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.an('object');
+                res.body.should.have.property('error');
+                res.body.error.should.have.property('code');
+                res.body.error.code.should.be.eql(2);
+                
+                done();
+            });
+        });
     });
     
     /*
