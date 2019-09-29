be5657c21e2855091d2ed55c7494c22477f4dfd1
LDanielMedinaC <A01651070@itesm.mx>
2019-09-29 15:23:02 -0500

    3 of 15 written test, not run it yet

---
+++ test/tutoring.spec.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+const userController = require('../server/controllers').user;
+const userModel = require('../server/models').user;
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = 'localhost:8000';
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+
+
+describe('POST /user', () => {
+    let tutoring = {
+        date: "10/11/2019",
+        lat: 19.019635,
+        long: -98.246918,
+        locationType: 'Casa del tutor',
+        locationName: 'Tutor\'s place',
+        topicID: '5d8d49a56ee837016abcd2a7',
+        tutorID: '5d8d49a96ee837016abcd2b1',
+        userID: '5d8d49a56ee837016abcd2aa',
+        startTime: "12:00",
+        endTime: "23:00",
+        notes: 'It\'s not the best student',
+        paymentMethod: 'cash'
+    };
+    let attributeNames = ['date', 'lat','long','locationType', 'locationName', 'topicID', 'tutorID', 'userID','startTime', 'endTime', 'notes', 'paymentMethod']; 
+    let attributesValue = [];
+    it('shouldCreate a topic', () => {
+        chai.request(server)
+        .post('/topics')
+        .send(tutoring)
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            attributeNames.forEach(function(attributeName){
+                res.body.should.have.property(attributeName);
+            });
+            res.body.date.should.be.eql('10/11/2019');
+            res.body.lat.should.be.eql(19.019635);
+            res.body.long.should.be.eql(-98.246918);
+            res.body.locationType.should.be.eql('Casa del tutor');
+            res.body.locationName.should.be.eql('Tutor\'s place');
+            res.body.topicID.should.be.eql('5d8d49a56ee837016abcd2a7'); 
+            res.body.tutorID.should.be.eql('5d8d49a96ee837016abcd2b1'); 
+            res.body.userID.should.be.eql('5d8d49a56ee837016abcd2aa'); 
+            res.body.startTime.should.be.eql('12:00'); 
+            res.body.endTime.should.be.eql('23:00'); 
+            res.body.notes.should.be.eql('It\'s not the best student'); 
+            res.body.paymentMethod.should.be.eql('cash'); 
+            done();
+        });
+    });
+
+    it('No date was provided.', (done) => {
+        let tutoringAux = tutoring;
+        delete tutoringAux.date; 
+        chai.request(server)
+          .post('/users')
+          .send(new_user)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('error');
+            res.body.error.should.have.property('code');
+            res.body.error.code.should.be.eql(1); 
+            done();
+          });
+      });
+
+    it('Wrong date format.', (done) => {
+        let tutoringAux = tutoring;
+        tutoringAux.date = "19/20/4"; 
+        chai.request(server)
+          .post('/users')
+          .send(new_user)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('error');
+            res.body.error.should.have.property('code');
+            res.body.error.code.should.be.eql(2); 
+            done();
+        });
+    });
+
+    it('date provided is in past.', (done) => {
+        let tutoringAux = tutoring;
+        tutoringAux.date = '10/3/1999'; 
+        chai.request(server)
+          .post('/users')
+          .send(new_user)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('error');
+            res.body.error.should.have.property('code');
+            res.body.error.code.should.be.eql(3); 
+            done();
+        });
+    });
+
+});
