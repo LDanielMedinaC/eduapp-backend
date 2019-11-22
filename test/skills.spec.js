06415717ab4eee6a11952325b5ebb5965ec3c718
Syul968 <syul.pgn@gmail.com>
2019-11-22 01:06:47 -0600

    ids validator[skills CRUD]: Allow missing ids

--- test/skills.spec.js
+++ test/skills.spec.js
@@ -17,39 +17,45 @@ chai.use(chaiHttp);
 /*
     Validate IDs
     Requires specifying which method is to be used. Values allowed: 'GET',
-    'POST', 'PUT', 'DELETE'
+    'POST', 'PUT', 'DELETE'.
 */
 const validateIds = (method, routePattern, validIds) => {
-    // Extract ids to validate in route pattern
-    const idRegex = /(:\w+Id)/;
-    const idRegexGlobal = /(:\w+Id)/g;
-    const modelRegex = /(?:\:)(\w+)(?:Id)/;
-    let ids = routePattern.match(idRegexGlobal);
+    const idParamRegex = /(:\w+Id)/;
+    const idRegexGlobal = /(?:\:)(\w+Id)/g;
+    const modelRegex = /(\w+)(?:Id)/;
 
-    // For each id to validate: remove, replace for non-ObjectID, use
-    // non-findable id
+    // Parse id parameter names, populate array
+    let ids = [];
+    let newIdMatch;
+    while(newIdMatch = idRegexGlobal.exec(routePattern))
+        ids.push(newIdMatch[1]);
+
+    // For each id to validate:
+    //      - remove
+    //      - replace for non-ObjectID
+    //      - use not-found id
     for(let id of ids) {
         // Test with id removed
         let model = modelRegex.exec(id)[1];
-        let route = routePattern.replace(id, '');
+        let route = routePattern.replace(`:${id}`, '');
 
-        for(let key in validIds)
-            route = route.replace(`:${key}`, validIds[`${key}`]);
+        // Test with id removed only if 'canBeMissing' is not enabled
+        if(validIds[id] && !validIds[id].canBeMissing) {
+            for(let key in validIds)
+                route = route.replace(`:${key}`, validIds[key].id || validIds[key]);
 
-        it(`No ${model} id [${route}]`, (done) => {
-            if(idRegex.test(route))
-                throw new Error(`Provided ids do not match route pattern: ${route}`);
+            it(`No ${model} id [${route}]`, (done) => {
+                if(idParamRegex.test(route))
+                    throw new Error(`Provided ids do not match route pattern: ${route}`);
 
-            switch(method) {
-                case 'GET':
-                    chai.request(server)
-                    .get(route)
-                    .end((err, res) => {
-                        shouldBeError(res, done, Errors.ROUTE_ERROR);
-                    });
-                    break;
-            }
-        });
+                let chaiMethod = method.toLowerCase();
+                chai.request(server)
+                [chaiMethod](route) // If this line fails you are using a not allowed HTTP method
+                .end((err, res) => {
+                    shouldBeError(res, done, Errors.ROUTE_ERROR);
+                });
+            });
+        }
     }
 };
 
@@ -67,11 +73,19 @@ describe('SKILLS', () => {
         });
     });
 
+    /*
+    * Test get skill details
+    * GET /tutors/:tutorId/skills/:skillId
+    * Retrieve tutor skill
+    */
     const getPattern = '/tutors/:tutorId/skills/:skillId';
     describe(`GET ${getPattern}`, () => {
         validateIds('GET', getPattern, {
             tutorId: '5db48a252f3af03923defe82',
-            skillId: '5db88a888f8af88888defe88'
+            skillId: {
+                id: '5db88a888f8af88888defe88',
+                canBeMissing: true
+            }
         });
     });
 });
